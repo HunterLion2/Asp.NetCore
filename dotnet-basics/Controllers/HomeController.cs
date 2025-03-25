474b541941e60e5c8b52aba04191e9667c06ccbd
Çınar Özgürgil <cozgurgil@gmail.com>
2025-03-25 22:57:40 +0300

    Default-Action-Methods-Mantığı

--- dotnet-basics/Controllers/HomeController.cs
+++ dotnet-basics/Controllers/HomeController.cs
@@ -4,11 +4,25 @@ namespace dotnet_basics.Controllers;
 
 public class HomeController: Controller {
 
-    // localhost:5158/home
+    // localhost:5158/ => Eğer direkt bu şekilde çağırırsak programın kendine belirlemiş olduğu değerler gelir o değerlerde Program.cs de belirlidir.
+    // Burada "app.MapControllerRoute" içerisinde otomatik olarak gelicek ana değer "Home" ondan sonra gelicek değer ise "Index" diye belirlidir. 
+
+    // localhost:5158/home => Eğer bu şekilde çağırırsak o zaman programın kendine tanımlı olan "Index" değerini getirir biz onun burada içerisini doldurduğumuz için buradaki Index() değeri gelir.
+    public string Index() {
+        return "Index";
+    }
+
+
     // localhost:5158/home/about => Burada bunu ekleriz.
 
     public string About() {
         return "About";
     }
+
+    // localhost:5158/home/contact => Burada bunu ekleriz.
+    public string Contact() {
+        return "About";
+    }
+
 
 }
