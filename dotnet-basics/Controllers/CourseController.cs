4d314edfb32c936a3fbe4976e8a15c45e4fbe7c9
Çınar Özgürgil <cozgurgil@gmail.com>
2025-03-29 13:15:27 +0300

    Models

--- dotnet-basics/Controllers/CourseController.cs
+++ dotnet-basics/Controllers/CourseController.cs
@@ -2,23 +2,57 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_basics.Controllers;
-    public class CourseController: Controller
+public class CourseController : Controller
+{
+
+
+    // localhost:5158/home
+    // localhost:5158/home/list => Burada bunu ekleriz.
+
+    public ActionResult Index()
     {
-        
-
-        // localhost:5158/home
-        // localhost:5158/home/list => Burada bunu ekleriz.
-
-        public ActionResult Index() {
-            return View();
-        }
-
-        public ActionResult List() {
-            return View();
-        }
-
-        public ActionResult Details() {
-            return View();
-        }
-
+        return View();
     }
+
+    public ActionResult List()
+    {
+        // string kursAdi1 = "Javascript Kursu";
+        // string kursAdi2 = "React Kursu";
+
+        // string kursAciklama1 = "Javascript Kurs Açıklaması";
+        // string kursAciklama2 = "React Kurs Açıklaması";
+
+        string[] kursAdlari = ["Javascript Kursu", "React Kursu"];
+        string[] kursAciklamalari = ["Javascript Kurs Açıklaması", "React Kurs Açıklaması"];
+        string[] kursResimleri = ["1.jpg", "2.jpg"];
+
+        ViewData["kursAdi1"] = kursAdlari[0]; // Javascript Kursu
+        ViewData["kursAdi2"] = kursAdlari[1]; // React Kursu
+
+        ViewData["kursAciklama1"] = kursAciklamalari[0]; // Javascript Kurs Açıklaması
+        ViewData["kursAciklama2"] = kursAciklamalari[1]; // React Kurs Açıklaması
+
+        ViewData["kursResim1"] = kursResimleri[0]; // 1.jpg
+        ViewData["kursResim2"] = kursResimleri[1]; // 2.jpg
+
+        // ViewData["kursAdlari"] = kursAdlari;
+        // ViewData["kursAciklamalari"] = kursAciklamalari;
+        // ViewData["kursResimleri"] = kursResimleri;
+        // @(((string[])ViewData["kursAdlari"])[0])
+
+        return View();
+    }
+
+    public ActionResult Details()
+    {
+        // string kursAdi = "Django Kursu";
+        int kursSaati = 40;
+        // bool yayindami = true;
+        string[] kursAdlari = ["Javascript Kursu", "React Kursu"];
+
+        ViewData["kursSaati"] = kursSaati;
+
+        return View(kursAdlari);
+    }
+
+}
--- dotnet-basics/obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
+++ dotnet-basics/obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd84c7fc0ae9315891d920e137583ab3dc2c5937")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d95ef2b9dea6accb00b0e6dc823821db9bb388e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
